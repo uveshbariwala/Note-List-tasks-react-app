eef70bddcd191d6aaed7050001731f5ffc0991d2
Uvesh Bariwala <uveshbariwala2129@gmail.com>
2022-02-23 15:34:00 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {useState, useEffect} from "react";
+import './App.css';
+import Header from './components/Header';
+import Form from './components/Form';
+import TodoList from './components/Todolist';
+const App = () => {
+
+  const initialstate = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);  
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos));
+  },[todos]);
+
+  return (
+      <div className = "container">
+        <div className='app-wrapper'>
+          <div>
+            <Header/>
+          </div>
+          <div>
+            <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+            />
+          </div>
+          <div>
+            <TodoList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo}/>
+          </div>
+        </div> 
+      </div>
+    );
+}
+
+export default App;
